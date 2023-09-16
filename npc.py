30e9a4760aa8ce951cd9792387a746a082411de2
remulak3 <remulak3@gmail.com>
2023-09-16 15:50:57 +0200

    Add HalfElf race

--- npc.py
+++ npc.py
@@ -6,29 +6,25 @@ from races.elf import Elf
 from races.halfling import Halfling
 from races.dragonborn import Dragonborn
 from races.gnome import Gnome
+from races.halfelf import HalfElf
+
 
 class Npc:
     ability_scores = AbilityScores()
+
     def __init__(self) -> None:
         self.race = {}
         self.generate_race()
         self.apply_race_asi()
 
     def generate_race(self):
-        available_races = [
-            Dwarf,
-            Human,
-            Elf,
-            Halfling,
-            Dragonborn,
-            Gnome
-        ]
+        available_races = [Dwarf, Human, Elf, Halfling, Dragonborn, Gnome, HalfElf]
         self.race = random.choice(available_races)
 
     def print_statblock(self) -> None:
         for name, value in self.ability_scores.scores.items():
-            print(name,":",value)
-  
+            print(name, ":", value)
+
     def print_npc_info(self) -> None:
         self.ability_scores.print_ability_scores_bonuses()
         self.race.describe_race()
