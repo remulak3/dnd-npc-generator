2f9bf3368e0cd5a274bfa7d72100f864709756e7
remulak3 <remulak3@gmail.com>
2023-09-16 20:50:54 +0200

    Rename class to python friendly

--- npc_class.py
+++ npc_class.py
@@ -1,23 +1,24 @@
-class Class:
+import random
+
+
+class NpcClass:
     def __init__(self) -> None:
         self.name = ""
         self.hit_dice = 0
         self.level = 1
         self.starting_hit_points = 0
-        self.hitpoints = self.starting_hit_points
         self.armor_proficiencies = []
         self.weapon_proficiencies = []
         self.tools_proficiences = []
         self.saving_throws_proficiencies = []
         self.skill_proficiencies = []
-        self.starting_equipment = {}
+        self.starting_equipment = []
         self.class_abilities = {}
 
     def describe_class(self):
         print("Class: ", self.name)
         print("Level:", self.level)
         print("Hit die: ", self.hit_dice)
-        print("Hitpoints: ", self.hitpoints)
         print("Armor Proficiencies:")
         for proficiency in self.armor_proficiencies:
             print("-", proficiency)
@@ -35,7 +36,15 @@ class Class:
             print("-", proficiency)
         if len(self.starting_equipment) > 0:
             print("Equipment:")
-            for name, description in self.starting_equipment.items():
-                print(" - ", name, ":", description)
+            for item in self.starting_equipment:
+                print("-", item)
         for ability_name, ability_description in self.class_abilities.items():
             print(" - ", ability_name, ":", ability_description)
+
+    @staticmethod
+    def pick_x_random_skills(count, list_of_skills):
+        chosen_skills = []
+        random.shuffle(list_of_skills)
+        for i in range(count):
+            chosen_skills.append(list_of_skills[i])
+        return chosen_skills
