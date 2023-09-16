ebc85dc001fa634a9378d8ba142a2a92ec9aaa81
remulak3 <remulak3@gmail.com>
2023-09-16 03:06:33 +0200

    Add generate ability scores test

--- tests/test_ability_score.py
+++ tests/test_ability_score.py
@@ -8,3 +8,10 @@ def test_calculate_ability_bonus_negative():
 
 def test_calculate_ability_bonus_positive():
     assert AbilityScores.calculate_ability_bonus(18) == "+4"
+
+def test_generate_ability_scores_has_table_values():
+    test_scores = AbilityScores()
+    assert 8,10 in test_scores.scores.values()
+    assert 12,13 in test_scores.scores.values()
+    assert 15 in test_scores.scores.values()
+    
