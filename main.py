302af1e5e36dec43839215bc93fa5c4300931cf6
remulak3 <remulak3@gmail.com>
2023-09-20 21:06:27 +0200

    Add statblock json file generation

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from npc import Npc
+from statblock import convert_npc_to_statblock_json
 
 mariusz = Npc(level=3)
-mariusz.print_npc_info()
+# mariusz.print_npc_info()
+
+with open("statblock.monster", "w") as outfile:
+    outfile.write(convert_npc_to_statblock_json(mariusz))
